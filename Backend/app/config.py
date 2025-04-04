795757fff5c6824c73fd8ada1040d68a306902bd
GabrielJim1004 <gabrieljim1004@gmail.com>
2025-04-03 19:53:22 -0600

    actualizacion backend

--- Backend/app/config.py
+++ Backend/app/config.py
@@ -14,7 +14,7 @@ class Settings(BaseSettings):
     # JWT Settings
     JWT_SECRET_KEY: str = os.getenv("JWT_SECRET_KEY", "supersecretkey")
     JWT_ALGORITHM: str = "HS256"
-    JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+    JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 
     
     # Database Settings
     DATABASE_URL: str = os.getenv(
--- Backend/app/database.py
+++ Backend/app/database.py
@@ -1,21 +1,25 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from .config import settings
+#Configura la base de datos y proporciona las herramientas necesarias para interactuar con ella.
+from sqlalchemy import create_engine #crea la conexión a la base de datos
+from sqlalchemy.ext.declarative import declarative_base #Define la clase base para los modelos de la base de datos
+from sqlalchemy.orm import sessionmaker #crea la sesión para interactuar con la base de datos
+from .config import settings #Importa la configuración de la base de datos desde el archivo de configuración
 
-# Create database engine
+# Crea el motor de la base de datos utilizando la URL de la base de datos definida en la configuración
 engine = create_engine(settings.DATABASE_URL)
 
-# Create SessionLocal class
+# Crea una clase de sesión que se utilizará para interactuar con la base de datos
+# autocommit=False: No se guardan automáticamente los cambios en la base de datos
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Create Base class
+# Crea una clase base para los modelos de la base de datos
+# Esta clase se utilizará para definir los modelos de la base de datos
 Base = declarative_base()
 
-# Database dependency
+# Crea una función que proporciona una sesión de base de datos para su uso en las rutas de la API
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+        
